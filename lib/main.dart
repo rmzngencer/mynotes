3ccc904e8b9d8b1183d5070834fc157c8ad8c3fe
rmzngencer <gncrgs@gmail.com>
2023-12-17 09:08:41 +0300

    register viwes and home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
-
-
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
-import 'firebase_options.dart';
+import 'package:mynotes/views/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -15,42 +14,19 @@ void main() {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginView(),
+      home: const HomePage(),
     ),
   );
 }
 
-
-class RegisterView extends StatefulWidget {
-  const RegisterView({super.key});
-
-  @override
-  State<RegisterView> createState() => _RegisterViewStateState();
-}
-
-class _RegisterViewStateState extends State<RegisterView> {
-
-  late final TextEditingController _email;
-  late final TextEditingController _password;
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
-  void initState() {
-    _email = TextEditingController();
-    _password = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _email.dispose();
-    _password.dispose();
-    super.dispose();
-  }
-   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Register'),
+        title: const Text('Home Page'),
         backgroundColor: const Color.fromARGB(255, 194, 180, 218),
       ),
       body: FutureBuilder(
@@ -60,54 +36,19 @@ class _RegisterViewStateState extends State<RegisterView> {
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return Column(
-                children: [
-                  TextField(
-                    controller: _email,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your email',
-                    ),
-                  ),
-                  TextField(
-                    controller: _password,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter your password',
-                    ),
-                  ),
-                  TextButton(
-                    onPressed: () async {
-                      final email = _email.text;
-                      final password = _password.text;
-                      
-
-                      
-
-
-                      final userCredential = await FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                        email: email,
-                        password: password,
-                      );
-
-                      print(userCredential);
-                    },
-                    child: const Text('Register', style: TextStyle(color: Color.fromARGB(255, 148, 20, 20)),),
-                
-                  ),
-                ],
-              );
+              final user = FirebaseAuth.instance.currentUser;
+              
+                if (user?.emailVerified ?? false){
+                  print("Yu are virified user");
+                }else{
+                  print("You are not verified user");
+                }
+              return const Text("dane");
             default:
-              return Text("loading..");
+              return const Text("loading");
           }
         },
       ),
     );
   }
 }
-
