8644b806cf2d9cf10d208f13b113a6c406139783
rmzngencer <gncrgs@gmail.com>
2023-12-19 10:24:33 +0300

    logim error

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'dart:developer' as devtools show log;
 
 import 'package:mynotes/constans/routes.dart';
-
+import 'package:mynotes/utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -32,77 +32,84 @@ class _LoginViewState extends State<LoginView> {
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Login'),
         backgroundColor: const Color.fromARGB(255, 194, 180, 218),
-        ),
-         
-     body: Column(
-                  children: [
-                    TextField(
-                      controller: _email,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter your email',
-                      ),
-                    ),
-                    TextField(
-                      controller: _password,
-                      obscureText: true,
-                      enableSuggestions: false,
-                      autocorrect: false,
-                      decoration: const InputDecoration(
-                        hintText: 'Enter your password',
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () async {
-                        final email = _email.text;
-                        final password = _password.text;
-     
-                        try {
-                        await FirebaseAuth.instance.signInWithEmailAndPassword(
-                            email: email,
-                            password: password,
-                          );
-                          // ignore: use_build_context_synchronously 
-                          Navigator.of(context).pushNamedAndRemoveUntil(
-                            notesRoute,
-                            (routes) => false,
-                          );                          
-                        } on FirebaseAuthException catch (e) {
-                          if (e.code == 'user-not-found') {
-                            devtools.log('No user found for that email.');
-                          } else if (e.code == 'wrong-password') {
-                            devtools.log('Wrong password provided for that user.');
-                          } else {
-                            devtools.log("hata: $e");
-                          }
-                        }
-                      },
-                      child: const Text(
-                        'login',
-                        style: TextStyle(color: Color.fromARGB(255, 148, 20, 20)),
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: (){
-                        Navigator.of(context).pushNamedAndRemoveUntil(
-                          registerRoute, 
-                          (route) => false
-                          );
-                      }, 
-                      child: const Text("not register yet?register here"))
-                  ],
-                ),
-   );
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _email,
+            enableSuggestions: false,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              hintText: 'Enter your email',
+            ),
+          ),
+          TextField(
+            controller: _password,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              hintText: 'Enter your password',
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              final email = _email.text;
+              final password = _password.text;
+
+              try {
+                await FirebaseAuth.instance.signInWithEmailAndPassword(
+                  email: email,
+                  password: password,
+                );
+                // ignore: use_build_context_synchronously
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                  notesRoute,
+                  (routes) => false,
+                );
+              } on FirebaseAuthException catch (e) {
+                if (e.code == 'user-not-found') {
+                  await showErrorDialog(
+                    context,
+                    'user not found.',
+                  );
+                } else if (e.code == 'wrong-password') {
+                  await showErrorDialog(
+                    context,
+                    'wrong pasworld.',
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
+                }
+              }catch(e){
+                await showErrorDialog(
+                    context,
+                    'Error: ${e.toString()}',
+                  );
+              }
+            },
+            child: const Text(
+              'login',
+              style: TextStyle(color: Color.fromARGB(255, 148, 20, 20)),
+            ),
+          ),
+          TextButton(
+              onPressed: () {
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil(registerRoute, (route) => false);
+              },
+              child: const Text("not register yet?register here"))
+        ],
+      ),
+    );
   }
 }
-  // late  mean is I am not ready for this variable now but I will be ready later
 
-  
-
- 
